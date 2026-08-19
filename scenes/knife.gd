85952be5f90218d2cdd606bc2f3184a9c10a0188
Oscar <robloxgamingindonesia7@gmail.com>
2026-08-19 21:49:26 +0700

    Added knife

---
+++ scenes/knife.gd
@@ -0,0 +1,7 @@
+extends StaticBody3D
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		animation_player.play("slash")
