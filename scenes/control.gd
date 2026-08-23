777ebd5df80b420b3fb7c7a9192376abe03ba38d
EggTree124 <robloxgamingindonesia7@gmail.com>
2026-08-23 22:10:27 +0700

    Update control.gd

--- scenes/control.gd
+++ scenes/control.gd
@@ -3,7 +3,6 @@ extends Control
 var score = 0
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	
 	if Input.is_action_just_pressed("ui_accept"):
 		score+=1
 		$Panel/HBoxContainer/RichTextLabel.text = str(score)
