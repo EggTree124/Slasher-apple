f0658082edc366ace7a741064b5ad04d9f79d3a2
Oscar Ho <robloxgamingindonesia7@gmail.com>
2026-08-26 14:46:08 +0700

    Added sfx

--- scenes/knife.gd
+++ scenes/knife.gd
@@ -1,9 +1,12 @@
 extends AnimatableBody3D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-
+@onready var slash: AudioStreamPlayer3D = $Cube/Slash
 func _ready() -> void:
 	add_to_group("knife")
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
-		animation_player.play("slash")
+		if slash and slash.stream:
+			slash.play()
+			if animation_player and animation_player.has_animation("slash"):
+				animation_player.play("slash")
